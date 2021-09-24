7e06a35b637b9ca8fd5712388b86c79242b674db
TheNetizen <65389667+TheNetizen@users.noreply.github.com>
2021-09-24 19:21:37 +0530

    good to go

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-var count = 1;
+
 class App extends React.Component {
         render() {
-                React.Children.map(this.props.children, () => {
-                        console.log('child', count++);
-                });
-                return (<h1>Hello</h1>)
+                return(<React.Fragment></React.Fragment>)
         }
 };
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App >
-    <li>List element</li>
-      <h3>Heading element</h3>
-      <p>Paragraph element</p>
-      <span>Span element</span>
-</App>,document.getElementById('root'));
+ReactDOM.render(<App/ >,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
